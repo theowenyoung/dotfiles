c17d5ec5e200a358dab6daf59209c30255c208d0
Owen Young <theowenyoung@gmail.com>
2022-06-29 00:54:18 +0800

    fix: debian install

---
+++ modules/jq/install_jq_debian.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+apt install jq -y
--- scripts/1-install-modules-for-debian.sh
+++ scripts/1-install-modules-for-debian.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+ENV_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/env.sh
 
+# import common env, like ROOT_DIR
+source $ENV_PATH;
+
+
+local module_path=$ENV_PATH/modules;
+
+$module_path/helix/install_helix_debian.sh
+
+$module_path/jq/install_jq_debian.sh
+
+$module_path/shadowsocks-rust/install_shadowsocks-rust.sh
