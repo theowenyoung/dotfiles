cf05440ab9d21cd1161e5ebd92fce514c1ef54a2
Owen Young <theowenyoung@gmail.com>
2022-06-28 22:17:59 +0800

    fix: debian init script

--- scripts/0-install-pre-required-modules-for-debian.sh
+++ scripts/0-install-pre-required-modules-for-debian.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 USERNAME="green"
-sudo apt update & sudo apt upgrade
+apt update & apt upgrade
 
+
+apt install --yes sudo
 # install git
-sudo apt install --yes git
+apt install --yes git
 # install zsh
 
-sudo apt install --yes zsh
+apt install --yes zsh
 
 # install keepassxc
 
-sudo apt install --yes keepassxc
+apt install --yes keepassxc
 
 # set zsh as the default shell
 
@@ -19,18 +21,18 @@ chsh -s $(which zsh)
 
 # change root password 
 
-sudo passwd $(whoami)
+passwd $(whoami)
 
 # create normal user
-sudo adduser $USERNAME
+adduser $USERNAME
 
-# change normal user to sudo 
+# change normal user to 
 
-sudo usermod -aG sudo $USERNAME
+usermod -aG $USERNAME
 
 echo create user $USERNAME success, now we will switch to user $USERNAME
 
-sudo su $USERNAME
+su $USERNAME
 
 
 # change zsh as the default shell
