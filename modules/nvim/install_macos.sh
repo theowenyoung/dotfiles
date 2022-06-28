d68d0e0f5e8a4dcf4e907dd6f142468557a0c226
Owen Young <theowenyoung@gmail.com>
2022-06-29 01:11:29 +0800

    fix: debian install modules

--- modules/nvim/install_macos.sh
+++ modules/nvim/install_macos.sh
@@ -2,6 +2,5 @@
 sudo port selfupdate
 sudo port -N install neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim  /opt/local/share/nvim/site/pack/packer/start/packer.nvim
-sudo port install fzf
 sudo port install ripgrep
 sudo port -N install ctags
--- scripts/1-install-modules-for-debian.sh
+++ scripts/1-install-modules-for-debian.sh
@@ -5,7 +5,7 @@ ENV_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/en
 source $ENV_PATH;
 
 
-module_path=$ENV_PATH/modules;
+module_path=$ROOT_DIR/modules;
 
 $module_path/helix/install_helix_debian.sh
 
