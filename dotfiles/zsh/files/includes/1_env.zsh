da7d8d092af17dc226e42e8a8314ef8a9357b7c0
Owen Young <theowenyoung@gmail.com>
2022-06-29 03:53:44 -0400

    ssh agent

--- dotfiles/zsh/files/includes/1_env.zsh
+++ dotfiles/zsh/files/includes/1_env.zsh
@@ -8,6 +8,10 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 
+# ssh agent for debian
+
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+
 # fzf
 export FZF_DEFAULT_COMMAND="fd --exclude={.git,.idea,.vscode,.sass-cache,node_modules,build} --type f"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
