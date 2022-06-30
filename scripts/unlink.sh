1972443289d323334597d3355283d01d51dcd81b
Owen Young <theowenyoung@gmail.com>
2022-06-30 16:48:53 +0800

    add unlink shell

---
+++ scripts/unlink.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+rm -rf ~/.zsh
+rm ~/.zshrc
+rm ~/.zshenv
+rm ~/.tmux.conf
+rm ~/.gitignore
+rm ~/.gitconfig
+rm -rf ~/.tmux
+rm -rf ~/.ssh
+rm -rf ~/.config/nvim
+rm -rf ~/.config/helix/config.toml
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/zellij
+rm -rf ~/.config/kak
+rm -rf ~/.config/wezterm
+rm -rf ~/.config/systemd
+rm -rf ~/.config/ss
+rm -rf ~/.config/gost
