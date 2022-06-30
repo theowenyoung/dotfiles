00e94d30c57598aecf1a93df8e91f669005f1cfc
Owen Young <theowenyoung@gmail.com>
2022-06-30 17:06:57 +0800

    fix helix debian runtime

--- modules/helix/install_helix_debian.sh
+++ modules/helix/install_helix_debian.sh
@@ -3,3 +3,8 @@
 git clone https://github.com/helix-editor/helix /tmp/helix
 cd /tmp/helix
 cargo install --path helix-term
+# copy runtime
+
+# remove now runtime
+rm -rf ~/.config/helix/runtime
+cp -r $PWD/runtime ~/.config/helix/runtime
