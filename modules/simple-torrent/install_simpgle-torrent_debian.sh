7065a2d7a78128f571f2e0af0d4c5209989fb96a
Owen Young <theowenyoung@gmail.com>
2022-07-01 13:58:48 +0800

    add simple torrent

---
+++ modules/simple-torrent/install_simpgle-torrent_debian.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    json_pp |
+    grep '"tag_name" :' |                                            # Get tag line
+    sed -E 's/.*"v([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+NAME="simple-torrent"
+REPO_NAME="boypt/simple-torrent"
+latest_version=$(get_latest_release $REPO_NAME)
+echo  start install $REPO_NAME latest v${latest_version}
+cd /tmp
+file_name="cloud-torrent_linux_amd64_static"
+curl -O -L https://github.com/$REPO_NAME/releases/download/v${latest_version}/${file_name}.gz
+gzip -d ${file_name}.gz
+mv ${filename} ~/.local/bin/${NAME}
+
+# add execute permission
+chmod +x ~/.local/bin/${NAME}
