a4338141e3aac3fa405332025b93d8fd65f20a0e
Owen Young <theowenyoung@gmail.com>
2022-07-02 21:12:35 +0800

    fix backup qbittorrent

--- modules/qBittorrent/backup_qBittorrent_config.sh
+++ modules/qBittorrent/backup_qBittorrent_config.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-cp -f ~/.config/qBittorrent/qBittorrent.conf $DOTFILES_PRIVATE_PATH/qBittorrent/files/qBittorrent.conf
+sudo cp -f ~/.config/qBittorrent/qBittorrent/qBittorrent.conf $DOTFILES_PRIVATE_PATH/qBittorrent/files/qBittorrent.conf
+sudo chown $USERNAME:$USERNAME $DOTFILES_PRIVATE_PATH/qBittorrent/files/qBittorrent.conf
+
 echo backup success.
