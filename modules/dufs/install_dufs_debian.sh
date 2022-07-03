06999bb580b5e5587fdd1e6ebb9fa2f19a5865d3
Owen Young <theowenyoung@gmail.com>
2022-07-03 16:47:56 +0800

    add dufs

---
+++ modules/dufs/install_dufs_debian.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    json_pp |
+    grep '"tag_name" :' |                                            # Get tag line
+    sed -E 's/.*"v([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+NAME="dufs"
+REPO_NAME="sigoden/dufs"
+latest_version=$(get_latest_release $REPO_NAME)
+echo  start install $REPO_NAME latest v${latest_version}
+cd /tmp
+file_name="dufs-v$latest_version-x86_64-unknown-linux-musl.tar.gz"
+curl -O -L https://github.com/$REPO_NAME/releases/download/v${latest_version}/${file_name}.tar.gz
+tar -xf ${file_name}.tar.gz
+mv ${NAME} ~/.local/bin/${NAME}
+
+# add execute permission
+chmod +x ~/.local/bin/${NAME}
--- scripts/1_install_modules_for_debian.sh
+++ scripts/1_install_modules_for_debian.sh
@@ -31,3 +31,5 @@ $module_path/podman/install_podman_debian.sh
 $module_path/pip3/install_pip3_debian.sh
 
 $module_path/podman-compose/install_podman-compose.sh
+
+$module_path/dufs/install_dufs_debian.sh
