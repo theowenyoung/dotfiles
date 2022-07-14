5b598ba67426922f3e8819405584bf709b5f9ded
Owen Young <theowenyoung@gmail.com>
2022-07-14 11:10:15 +0800

    fix editorconfig

--- scripts/0_install_pre_required_modules_for_debian.sh
+++ scripts/0_install_pre_required_modules_for_debian.sh
@@ -1,63 +1,75 @@
 #!/bin/bash
 set -e
+
+# check is sudo 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
 USERNAME="green"
 
-sudo apt update & sudo apt upgrade --yes
+apt update && apt upgrade --yes
 
+apt install --yes sudo
 
-sudo apt install --yes sudo
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
 
 # install openssl for comtrya
-sudo apt install --yes pkg-config libssl-dev
+apt install --yes pkg-config libssl-dev
 # install git
-sudo apt install --yes git
+apt install --yes git
 # install zsh
 
-sudo apt install --yes zsh
+apt install --yes zsh
 
-sudo apt install --yes curl
 # install keepassxc
-sudo apt install --yes build-essential
+apt install --yes build-essential
 
-sudo apt install --yes keepassxc
+apt install --yes keepassxc
 
 # for comtrya depend openssl 1.1
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.6_amd64.deb
 
-sudo dpkg -i libssl1.1_1.1.1l-1ubuntu1.6_amd64.deb
+dpkg -i libssl1.1_1.1.1l-1ubuntu1.6_amd64.deb
 
 rm libssl1.1_1.1.1l-1ubuntu1.6_amd64.deb
 
 # create 4000 group
-sudo groupadd -g 4000 $USERNAME
+groupadd -g 4000 $USERNAME
 # create normal user
-sudo useradd -u 4000 -g 4000 -m -s /bin/zsh $USERNAME
+useradd -u 4000 -g 4000 -m -s /bin/zsh $USERNAME
 # change normal user to sudo 
 
-sudo usermod -aG sudo $USERNAME
+usermod -aG sudo $USERNAME
 
 # add user to no password group
 # https://superuser.com/posts/1027257/revisions
 echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" > ~/green_sudoers
-sudo visudo -c -q -f /home/$(whoami)/green_sudoers && \
-sudo chmod 440 /home/$(whoami)/green_sudoers && \
-sudo cp /home/$(whoami)/green_sudoers /etc/sudoers.d/green_sudoers
-sudo rm /home/$(whoami)/green_sudoers
+visudo -c -q -f /home/$(whoami)/green_sudoers && \
+chmod 440 /home/$(whoami)/green_sudoers && \
+cp /home/$(whoami)/green_sudoers /etc/sudoers.d/green_sudoers
+rm /home/$(whoami)/green_sudoers
 # change password
-sudo passwd $USERNAME
+passwd $USERNAME
 
 echo create user $USERNAME success
 
 echo please save it to keepassxc
 
 # create .zshrc
-sudo touch /home/$USERNAME/.zshrc
-sudo chown $USERNAME:$USERNAME /home/$USERNAME/.zshrc
+touch /home/$USERNAME/.zshrc
+chown $USERNAME:$USERNAME /home/$USERNAME/.zshrc
 
 # su to normal user to install rust
 
 echo we will switch to user $USERNAME
-sudo su - $USERNAME
+su - $USERNAME
 
 echo now you can run the following "command" to install dotfiles
 
