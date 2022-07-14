3824bf82afcc396cbecd431bde339b16319c777a
Owen Young <theowenyoung@gmail.com>
2022-07-14 13:45:15 -0400

    fix env

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-
-if [ "$SHELL" = "/bin/zsh" ]; then
+#!/bin/sh
+echo $SHELL;
+if [[ "$SHELL" == *"zsh"* ]] ; then
     ROOT_DIR=$( cd -- "$( dirname -- "${(%):-%N}" )"/../ &> /dev/null && pwd )
-elif [ "$SHELL" = "/bin/bash" ]; then
+elif [[ "$SHELL" == *"bash"* ]]; then
     ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/../ &> /dev/null && pwd )
 else
     echo "unknown shell"
