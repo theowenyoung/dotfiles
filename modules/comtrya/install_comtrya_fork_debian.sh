07dbf9f3f6f077773478352cc1b5dcbfc3f1d52e
Owen Young <theowenyoung@gmail.com>
2022-07-14 09:37:31 +0800

    fix install

--- modules/comtrya/install_comtrya_fork_debian.sh
+++ modules/comtrya/install_comtrya_fork_debian.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+set -e;
 cd -- /tmp
 
-curl -OL https://github.com/theowenyoung/comtrya/releases/download/untagged-01d38a3194294f8430e3/comtrya-x86_64-unknown-linux-musl
+curl -O -L https://github.com/theowenyoung/comtrya/releases/download/untagged-01d38a3194294f8430e3/comtrya-x86_64-unknown-linux-musl
 
 
 mv ./comtrya-x86_64-unknown-linux-musl ~/.local/bin/comtrya
