07795e242871fb678888a277beb51c7a4f06d7b1
Owen Young <theowenyoung@gmail.com>
2022-07-14 12:18:43 +0800

    binary install kakoune

--- modules/kakoune/install_kakoune_debian_binary.sh
+++ modules/kakoune/install_kakoune_debian_binary.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 set -e
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share/kak/
+# sudo apt install -y kakoune
 
-sudo apt install -y kakoune
+cd -- /tmp
+curl -O -L https://github.com/theowenyoung/kakoune/releases/download/v2021.11.08-2/kakoune-v2021.11.08-2-linux.tar.bz2
+
+tar -xf kakoune-v2021.11.08-2-linux.tar.bz2
+
+cp kakoune-v2021.11.08-2-linux/bin/kak ~/.local/bin
+
+cp -r kakoune-v2021.11.08-2-linux/share/kak ~/.local/share/kak
+
+echo "success"
