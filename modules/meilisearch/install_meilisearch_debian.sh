02301121cbb40d7a69dbb18d1d092e5b2ea62398
Owen Young <theowenyoung@gmail.com>
2022-07-16 07:56:28 +0800

    use 0.27.2

--- modules/meilisearch/install_meilisearch_debian.sh
+++ modules/meilisearch/install_meilisearch_debian.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget -O ~/.local/bin/meilisearch https://github.com/meilisearch/meilisearch/releases/download/v0.28.0/meilisearch-linux-amd64
+wget -O ~/.local/bin/meilisearch https://github.com/meilisearch/meilisearch/releases/download/v0.27.2/meilisearch-linux-amd64
 
 chmod 700 ~/.local/bin/meilisearch
