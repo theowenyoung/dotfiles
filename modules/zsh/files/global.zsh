526d773bd5f4befcca8f6f1d7da15d7b745d83ca
Owen Young <theowenyoung@gmail.com>
2022-08-06 01:25:44 +0800

    global xdg run

--- modules/zsh/files/global.zsh
+++ modules/zsh/files/global.zsh
@@ -60,9 +60,12 @@ fi
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_RUNTIME_DIR="/run/user/$UID"
 export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
 
+
+case "$OSTYPE" in
+	  linux*)    export XDG_RUNTIME_DIR="/run/user/$UID";;
+esac
 # ssh agent for debian
 
 export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
