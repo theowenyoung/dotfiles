34e4a92ba03a49f2222b0450bc1e919e813d037b
Owen Young <theowenyoung@gmail.com>
2022-08-11 21:12:37 +0800

    clash

--- root/clash/README.md
+++ root/clash/README.md
@@ -5,3 +5,4 @@
 ## Reference
 
 - [ubuntu clash](https://juejin.cn/post/7065548789997633567)
+- [clash sh](https://gist.github.com/cld4h/9a03ec2f826a25be5ab974fdbc540de4)
--- root/clash/files/start_clash_debian.sh
+++ root/clash/files/start_clash_debian.sh
@@ -14,27 +14,24 @@ sudo ln -sf /home/green/dotfiles/private/clash/files/config_linux.yaml /etc/opt/
 sudo ln -sf /home/green/dotfiles/private/clash/files/ruleset /etc/opt/clash/ruleset
 
 
+# check if bak exists
+if [ -f "/etc/resolv.conf.bak" ]; then
+  echo "file /etc/resolv.conf.bak exists, do nothing"
+else
+  echo "file /etc/resolv.conf.bak not exists, backup it"
+  sudo cp /etc/resolv.conf /etc/resolv.conf.bak
+fi
 
 # backup /etc/resolv.conf
-cp /etc/resolv.conf /etc/resolv.conf.bak
 
 # change the dns server to 127.0.0.1
-echo 'nameserver 127.0.0.1' > /etc/resolv.conf
 
-# change iptables
+dnsresolv=$(cat <<-END
+nameserver 127.0.0.1
+nameserver 119.29.29.29
+END
+)
+echo "$dnsresolv" > /etc/resolv.conf
 
-iptables -t nat -N CLASH
-iptables -t nat -A CLASH -d 10.0.0.0/8 -j RETURN
-iptables -t nat -A CLASH -d 127.0.0.0/8 -j RETURN
-iptables -t nat -A CLASH -d 169.254.0.0/16 -j RETURN
-iptables -t nat -A CLASH -d 172.16.0.0/12 -j RETURN
-iptables -t nat -A CLASH -d 192.168.0.0/16 -j RETURN
-iptables -t nat -A CLASH -d 224.0.0.0/4 -j RETURN
-iptables -t nat -A CLASH -d 240.0.0.0/4 -j RETURN
-iptables -t nat -A CLASH -p tcp -j REDIRECT --to-ports 7892
-
-sudo /sbin/iptables-save > /etc/iptables/rules.v4
-sudo /sbin/ip6tables-save > /etc/iptables/rules.v6
-cat /etc/iptables/rules.v4
 
 /opt/clash/bin/clash -d /etc/opt/clash -f /etc/opt/clash/config_linux.yaml
