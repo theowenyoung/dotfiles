f1d414d48f291748764ac11eed911e86c759d815
Owen Young <theowenyoung@gmail.com>
2022-08-11 16:27:13 +0800

    add deiban clash

---
+++ root/clash/files/start_clash_debian.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e;
+
+# check is root
+if [ "$(id -u)" != "0" ]; then
+    echo "Error: You must be root to run this script!"
+    exit 1
+fi
+# copy configs to /etc/opt/clash
+
+sudo mkdir -p /etc/opt/clash 
+
+sudo ln -s ~/dotfiles/private/clash/files/config_linux.yaml /etc/opt/clash/config_linux.yaml
+sudo ln -s ~/dotfiles/private/clash/files/ruleset /etc/opt/clash/ruleset
+
+/opt/clash/bin/clash -d /etc/opt/clash -f /etc/opt/clash/config_linux.yaml
+
+
+# backup /etc/resolv.conf
+cp /etc/resolv.conf /etc/resolv.conf.bak
+
+# change the dns server to 127.0.0.1
+echo 'nameserver 127.0.0.1' > /etc/resolv.conf
+
+# change iptables
+
+iptables -t nat -N CLASH
+iptables -t nat -A CLASH -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A CLASH -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A CLASH -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A CLASH -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A CLASH -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A CLASH -p tcp -j REDIRECT --to-ports 7892
+
+sudo /sbin/iptables-save > /etc/iptables/rules.v4
+sudo /sbin/ip6tables-save > /etc/iptables/rules.v6
+cat /etc/iptables/rules.v4
---
+++ root/clash/install_clash_premium_debian.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+NAME="clash"
+REPO_NAME="Dreamacro/clash"
+latest_version="2022.07.07"
+echo  start install $REPO_NAME latest v${latest_version}
+cd /tmp
+file_name=clash-linux-amd64-v3-${latest_version}
+# download_url="https://download.fastgit.org/Dreamacro/clash/releases/download/premium/${file_name}.gz";
+# download_url="https://github.com/Dreamacro/clash/releases/download/premium/${file_name}.gz";
+download_url="https://dotfiles.owenyoung.com/assets/${file_name}.gz";
+echo start download $download_url;
+# curl -O -L https://github.com/Dreamacro/clash/releases/download/premium/${file_name}.gz
+curl -O -L "$download_url"
+gzip -d ${file_name}.gz
+sudo mkdir -p /opt/clash/bin/
+sudo mv ${file_name} /opt/clash/bin/${NAME}
+
+# add execute permission
+sudo chmod +x /opt/clash/bin/${NAME}
+
+# copy ui
+sudo mkdir -p /etc/opt/caddy/
+cd -- /etc/opt/caddy/
+
+# check is ui exists
+if [ -d "/etc/opt/caddy/ui" ]; then
+  echo "ui exists"
+else
+  echo "ui not exists, we will clone it"
+  sudo git clone https://github.com/Dreamacro/clash-dashboard.git ui
+
+cd -- ui
+sudo git pull
+sudo git checkout gh-pages
+
+
+# create system server
+
+sudo cp ~/dotfiles/root/clash/files/start_clash_debian.sh /opt/clash/bin/start_clash_debian.sh
+sudo cp ~/dotfiles/root/clash/files/clash.service /etc/systemd/system/clash.service
+
+sudo ~/dotfiles/modules/systemd/restart_root_service.sh clash
