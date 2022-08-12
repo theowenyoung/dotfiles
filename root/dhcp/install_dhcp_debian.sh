585e8aec86e38c50f603198201a6944cc5c2cdc0
Owen Young <theowenyoung@gmail.com>
2022-08-13 03:57:01 +0800

    fix error

--- root/dhcp/install_dhcp_debian.sh
+++ root/dhcp/install_dhcp_debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+set -e;
 # copy files/etc/default/isc-dhcp-server to /etc/default/isc-dhcp-server
-sudo cp /home/green/dotfiles/root/dhcp/files/etc/default/isc_dhcp_server /etc/default/isc-dhcp-server
+sudo cp /home/green/dotfiles/root/dhcp/files/etc/default/isc-dhcp-server /etc/default/isc-dhcp-server
 
 # copy files/etc/dhcp/dhcpd.conf to /etc/dhcp/dhcpd.conf
 sudo cp /home/green/dotfiles/root/dhcp/files/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
