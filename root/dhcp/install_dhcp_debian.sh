6267056853b7303ddbdbd1c1686d7d4f0d26d326
Owen Young <theowenyoung@gmail.com>
2022-08-13 03:54:52 +0800

    add dhcp

---
+++ root/dhcp/install_dhcp_debian.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# copy files/etc/default/isc-dhcp-server to /etc/default/isc-dhcp-server
+sudo cp /home/green/dotfiles/root/dhcp/files/etc/default/isc_dhcp_server /etc/default/isc-dhcp-server
+
+# copy files/etc/dhcp/dhcpd.conf to /etc/dhcp/dhcpd.conf
+sudo cp /home/green/dotfiles/root/dhcp/files/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
+
+sudo apt install --yes isc-dhcp-server
