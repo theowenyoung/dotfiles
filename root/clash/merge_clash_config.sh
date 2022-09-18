8b67a5eea3ec52df33f39e9528937afd526293d2
Owen Young <theowenyoung@gmail.com>
2022-09-19 02:27:36 +0800

    clash

--- root/clash/merge_clash_config.sh
+++ root/clash/merge_clash_config.sh
@@ -2,16 +2,17 @@
 set -e;
 cd -- ~/dotfiles/private/clash/files/;
 
-a=$(yq -o=json -I=0 me.yaml); b=$(yq -o=json -I=0 b.yaml);
+a=$(yq -o=json -I=0 me.yaml);
+b=$(yq -o=json -I=0 b.yaml);
 linux=$(yq -o=json -I=0 me_linux.yaml);
 macos=$(yq -o=json -I=0 me_macos.yaml);
-
 premium_only=$(yq -o=json -I=0 premium_only.yaml);
-
+posts=$(yq -o=json -I=0 post.yaml)
 # echo "$premium_only";
 c=$(jq --argjson a "$a" --argjson b "$b" -n '{proxies:[$a.proxies[],$b.proxies[]]}');
 d=$(jq  --argjson a "$a" --argjson b "$b" -n '$a["proxy-groups"][:-1] as $arr1 | $a["proxy-groups"][-1].proxies as $proxies1 | $b["proxy-groups"][1].proxies as $proxies2 | {"proxy-groups": [$arr1[],{"name":"Proxy","type":"select","proxies":[$proxies1[],$proxies2[]]}] }');
-premiumu_rules=$(jq --argjson a "$a" --argjson premium_only "$premium_only" -n '{rules:[$premium_only.rules[],$a.rules[]]}');
+premiumu_rules=$(jq --argjson a "$a" --argjson premium_only "$premium_only" --argjson posts "$posts" -n '{rules:[$a.rules[],$premium_only.rules[],$posts.rules[]]}');
+simple_rules=$(jq --argjson a "$a" --argjson posts "$posts" -n '{rules:[$a.rules[],$posts.rules[]]}');
 
 
 jq  --argjson a "$a" --argjson c "$c" --argjson d "$d" --argjson premium_only "$premium_only" --argjson premiumu_rules "$premiumu_rules" --argjson macos "$macos" -n '$a * $c * $d * $premium_only * $premiumu_rules * $macos' | yq -P - > config.yaml
@@ -22,4 +23,4 @@ jq  --argjson a "$a" --argjson c "$c" --argjson d "$d"  --argjson premium_only "
 
 
 # also generate a simple config
-jq  --argjson a "$a" --argjson c "$c" --argjson d "$d" -n '$a * $c * $d' | yq -P - > config_normal.yaml
+jq  --argjson a "$a" --argjson c "$c" --argjson d "$d" --argjson simple_rules "$simple_rules" -n '$a * $c * $d * $simple_rules' | yq -P - > config_normal.yaml
