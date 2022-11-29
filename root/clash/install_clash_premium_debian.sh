bd3510ab56cfbc0c04ea9c956c08321014338c19
Owen Young <theowenyoung@gmail.com>
2022-11-29 21:32:04 +0800

    update clash version

--- root/clash/install_clash_premium_debian.sh
+++ root/clash/install_clash_premium_debian.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-set -e;
+set -e
 NAME="clash"
 REPO_NAME="Dreamacro/clash"
-latest_version="2022.07.07"
-echo  start install $REPO_NAME latest v${latest_version}
+latest_version="2022.11.25"
+echo start install $REPO_NAME latest v${latest_version}
 cd /tmp
 file_name=clash-linux-amd64-v3-${latest_version}
 # download_url="https://download.fastgit.org/Dreamacro/clash/releases/download/premium/${file_name}.gz";
 # download_url="https://github.com/Dreamacro/clash/releases/download/premium/${file_name}.gz";
-download_url="https://dotfiles.owenyoung.com/assets/${file_name}.gz";
+download_url="https://dotfiles.owenyoung.com/assets/${file_name}.gz"
 
 # check is file exists
 if [ -f "/tmp/${file_name}.gz" ]; then
-  echo "file ${file_name}.gz exists"
+    echo "file ${file_name}.gz exists"
 else
-  echo "file ${file_name}.gz not exists, download it"
-  echo start download $download_url;
-  curl -O -L "$download_url"
+    echo "file ${file_name}.gz not exists, download it"
+    echo start download $download_url
+    curl -O -L "$download_url"
 fi
 
 gzip -d ${file_name}.gz
@@ -32,15 +32,14 @@ cd -- /etc/opt/clash/
 
 # check is ui exists
 if [ -d "/etc/opt/clash/ui" ]; then
-  echo "ui exists"
-  # delete it
-  sudo rm -rf /etc/opt/clash/ui
+    echo "ui exists"
+    # delete it
+    sudo rm -rf /etc/opt/clash/ui
 fi
 sudo git clone https://github.com/theowenyoung/clash-dashboard.git ui
 cd -- ui
 sudo git pull
 sudo git checkout gh-pages
-
 
 # create system server
 
