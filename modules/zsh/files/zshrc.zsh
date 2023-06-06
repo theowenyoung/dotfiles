d288b9280bb6d3000a98063280b042a1d880cb97
Owen Young <theowenyoung@gmail.com>
2023-06-06 13:03:44 +0800

    update i

--- modules/zsh/files/zshrc.zsh
+++ modules/zsh/files/zshrc.zsh
@@ -34,3 +34,6 @@ export PATH="$PNPM_HOME:$PATH"
 # bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
