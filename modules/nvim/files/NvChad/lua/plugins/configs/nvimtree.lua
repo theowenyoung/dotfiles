5eaa79cbfb8ad2ddaab3ececb3a3b4b58cffc9e5
Owen Young <theowenyoung@gmail.com>
2023-10-02 19:11:05 +0800

    update nvim treim

--- modules/nvim/files/NvChad/lua/plugins/configs/nvimtree.lua
+++ modules/nvim/files/NvChad/lua/plugins/configs/nvimtree.lua
@@ -13,8 +13,6 @@ local options = {
   },
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = { "alpha" },
   hijack_cursor = true,
   hijack_unnamed_buffer_when_opening = false,
   update_cwd = true,
@@ -26,7 +24,6 @@ local options = {
     adaptive_size = true,
     side = "left",
     width = 25,
-    hide_root_folder = true,
   },
   git = {
     enable = false,
